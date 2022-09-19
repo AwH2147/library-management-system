fa1b96b4077f3969de83e26f3abb55c5d38541cd
AwH2147 <herathga@sltc.edu.lk>
2022-09-19 09:02:53 +0530

    First Commit

---
+++ admin/includes/footer.php
@@ -0,0 +1,11 @@
+   <section class="footer-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                   &copy; Online Library Management System |<a href="https://phpgurukul.com/" target="_blank"  > Designed by ICE Crew For Cloud Computing Module
+                   </a> 
+                </div>
+
+            </div>
+        </div>
+    </section>
